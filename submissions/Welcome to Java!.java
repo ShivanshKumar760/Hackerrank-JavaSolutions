06e6c486d0602bb6f0bf2ce43b5da45d1d226b4f
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-01-18 23:57:56 +0530

    added Welcome to Java!

---
+++ submissions/Welcome to Java!.java
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Welcome to Java!
+Problem Link: https://www.hackerrank.com/challenges/welcome-to-java/submissions/code/460341617
+Author: u
+Language : java15
+
+-----------------------------------------------------------------------*/
+
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
